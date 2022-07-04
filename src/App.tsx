123910d892a05589aa1cbd80abeaac15e6037fd4
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-04 13:06:37 +0200

    tac-tac-toe with numbers

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface SquareProps {
+	value: number;
+}
+
+class Square extends React.Component<SquareProps> {
+	constructor(props: SquareProps) {
+		super(props);
+		this.state = {value: null};
+	}
+	render() {
+		return (
+				//<button className="square" onClick={function() { console.log('click'); }} >
+				<button className="square" onClick={() => { console.log('click'); }} >
+				{this.props.value} 
+				{/* TODO */}
+				</button>
+			   );
+	}
+}
+
+function renderSquare(i: number) {
+	return <Square value={i}/>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TYPESCRIPT!!!! Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		<div>
+		{renderSquare(0)}
+		{renderSquare(1)}
+		{renderSquare(2)}
+		</div>
+		<div>
+		{renderSquare(3)}
+		{renderSquare(4)}
+		{renderSquare(5)}
+		</div>
+		<div>
+		{renderSquare(6)}
+		{renderSquare(7)}
+		{renderSquare(8)}
+		</div>
       </header>
     </div>
   );
