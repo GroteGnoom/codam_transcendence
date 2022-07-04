2b98ab0d8115abc0bde71e4159f95ecd5ce75838
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-04 13:47:25 +0200

    let board have state, instead of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,85 @@ import logo from './logo.svg';
 import './App.css';
 
 interface SquareProps {
-	value: number;
+	value: string;
+	onClick: () => void;
 }
 
-class Square extends React.Component<SquareProps> {
+interface SquareState {
+	value: string;
+}
+
+class Square extends React.Component<SquareProps, SquareState> {
 	constructor(props: SquareProps) {
 		super(props);
-		this.state = {value: null};
+		this.state = {value: ''};
 	}
 	render() {
 		return (
-				//<button className="square" onClick={function() { console.log('click'); }} >
-				<button className="square" onClick={() => { console.log('click'); }} >
-				{this.props.value} 
-				{/* TODO */}
-				</button>
-			   );
+			//<button className="square" onClick={function() { console.log('click'); }} >
+			<button className="square" onClick={() => { this.props.onClick()} } >
+			{this.props.value} 
+			{/* TODO */}
+			</button>
+		);
 	}
 }
 
-function renderSquare(i: number) {
-	return <Square value={i}/>;
+interface BoardProps {}
+interface BoardState {
+	squares: string[];
+
+}
+
+class Board extends React.Component<BoardProps, BoardState> {
+	constructor(props:BoardProps) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill('')
+		};
+	}
+	renderSquare(i: number) {
+		return (
+			<Square 
+			value={this.state.squares[i]}
+			onClick={() => this.handleClick(i)}
+			/>);
+	}
+	handleClick(i: number) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({squares: squares});
+	}
+	render() {
+		return (
+			<div className="App">
+			<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<div>
+			{this.renderSquare(0)}
+			{this.renderSquare(1)}
+			{this.renderSquare(2)}
+			</div>
+			<div>
+			{this.renderSquare(3)}
+			{this.renderSquare(4)}
+			{this.renderSquare(5)}
+			</div>
+			<div>
+			{this.renderSquare(6)}
+			{this.renderSquare(7)}
+			{this.renderSquare(8)}
+			</div>
+			</header>
+			</div>
+		);
+	}
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-		<div>
-		{renderSquare(0)}
-		{renderSquare(1)}
-		{renderSquare(2)}
-		</div>
-		<div>
-		{renderSquare(3)}
-		{renderSquare(4)}
-		{renderSquare(5)}
-		</div>
-		<div>
-		{renderSquare(6)}
-		{renderSquare(7)}
-		{renderSquare(8)}
-		</div>
-      </header>
-    </div>
-  );
+	return (
+		<Board />
+	)
 }
 
 export default App;
