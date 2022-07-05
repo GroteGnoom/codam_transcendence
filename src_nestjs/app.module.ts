64a8803f6d675a295917ec4882354080a6f3ea2e
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-05 15:17:15 +0200

    try to connect nestjs and postgres

--- src_nestjs/app.module.ts
+++ src_nestjs/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('DB_HOST'),
+				port: +configService.get<number>('DB_PORT'),
+				username: configService.get('DB_USERNAME'),
+				password: configService.get('DB_PASSWORD'),
+				database: configService.get('DB_NAME'),
+				entities: [],
+				synchronize: true,
+			}),
+			inject: [ConfigService],
+		}),
+		UsersModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
