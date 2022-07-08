e90b8da69bf67527c288705b78e60b835e4a66d7
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-08 15:55:40 +0200

    do post request via react

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
+class DoPost extends React.Component {
+	handle() {
+		fetch("http://127.0.0.1:5000/users/create", {
+			method: 'POST',
+			//mode: 'no-cors',
+			headers: {'Content-Type':'application/json'},
+			body: JSON.stringify({
+				username: "usernieuw",
+				password: "passpaaass",
+				email: "bla@bla.com",
+			})
+		})
+		.then(response => response.json())
+		.then(responseJson => {
+			console.log(responseJson);
+		});
+		console.log('gedaan');
+	}
+	render () {
+		return (
+			<form onSubmit={this.handle}>
+			<label>
+			Name:
+				<input type="text" name="name" />
+			</label>
+			<input type="submit" value="Submit" />
+			</form>
+		)
+	}
+}
+
 const Login = () =>
 {
 	return (
 		<main>
-			This page is for login
+		This page is for login
+			<DoPost />
 		</main>
 	)
 }
--- src_nestjs/main.ts
+++ src_nestjs/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.enableCors({
+	  origin: true,
+    "credentials": true,
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
   await app.listen(5000);
 }
 bootstrap();
