6033acf2027aafc4f30b1904e6fd22b29b25ecbe
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-11 12:31:55 +0200

    fix types of popup

--- src/Login.tsx
+++ src/Login.tsx
@@ -46,6 +46,8 @@ interface OAuthProps {
 
 //https://tasoskakour.com/blog/react-use-oauth2
 const OAUTH_STATE_KEY = 'react-use-oauth2-state-key';
+const POPUP_HEIGHT = 700;
+const POPUP_WIDTH = 600;
 
 // https://medium.com/@dazcyril/generating-cryptographic-random-state-in-javascript-in-the-browser-c538b3daae50
 const generateState = () => {
@@ -68,23 +70,62 @@ const saveState = (state: string) => {
 const removeState = () => {
 	sessionStorage.removeItem(OAUTH_STATE_KEY);
 };
+
+const openPopup = (url: string) => {
+	// To fix issues with window.screen in multi-monitor setups, the easier option is to
+	// center the pop-up over the parent window.
+	const top = window.outerHeight / 2 + window.screenY - POPUP_HEIGHT / 2;
+	const left = window.outerWidth / 2 + window.screenX - POPUP_WIDTH / 2;
+	return window.open(
+		url,
+		'OAuth2 Popup',
+		`height=${POPUP_HEIGHT},width=${POPUP_WIDTH},top=${top},left=${left}`
+	);
+};
+
+const enhanceAuthorizeUrl = (
+	authorizeUrl: string,
+	clientId: number,
+	redirectUri: string,
+	scope: string, //these two are probably not numbers
+	state: string
+) => {
+	return `${authorizeUrl}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}`;
+};
+
+/*
+const closePopup = (popupRef: number) => {
+	popupRef.current?.close();
+};
+*/
+
 const useOAuth2 = (props: OAuthProps) => {
-  const {
-      authorizeUrl,
-      clientId,
-      redirectUri,
+	const {
+		authorizeUrl,
+		clientId,
+		redirectUri,
       scope = '',
     } = props;
 
+	  var popupRef = React.useRef<Window | null>(null);
   const [{ loading, error }, setUI] = useState({ loading: false, error: null });
 
   const getAuth = useCallback(
-	  (newValue: string): void => setUI({
+	  (newValue: string): void => { setUI({
 		  loading: true,
 		  error: null,
-	  }),
-		  [setUI]
-	)
+	  });
+	  // 2. Generate and save state
+	  const state = generateState();
+	  saveState(state);
+
+	  // 3. Open popup
+	  popupRef.current = openPopup(
+		  enhanceAuthorizeUrl(authorizeUrl, clientId, redirectUri, scope, state)
+	  );
+	  },
+	  [setUI]
+  )
 }
 
 class DoOAuth extends React.Component {
