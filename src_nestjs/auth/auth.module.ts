1f941cb3b29bc97932c042e20b5d872ef1a4bf86
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-12 16:13:06 +0200

    add strategy to auth

--- src_nestjs/auth/auth.module.ts
+++ src_nestjs/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { UsersService } from '../users/services/users/users.service';
 import { UsersModule } from '../users/users.module';
+import { FtStrategy } from './ft.strategy'
 
 PassportModule.register({});
 
 @Module({
   imports: [],
-  providers: [AuthService]
+  providers: [AuthService, FtStrategy]
 })
 export class AuthModule {}
