93439309deaf391cd500623269296a96fce76b17
Lindsay Wray <lwray@f1r1s19.codam.nl>
2022-07-18 11:56:09 +0200

    .

--- react/src/Chat.tsx
+++ react/src/Chat.tsx
@@ -3,16 +3,17 @@ import './Chat.css';
 
 class ChannelList extends React.Component<{}, { 
     channels: any[], messages: any[], activeChannel: string 
-    text: string, settings: any }> {
+    text: string, settings: any, newChannel: string }> {
 
     constructor(props: any){
         super(props);
         this.state = { 
             channels: [], 
             messages: [], 
-            activeChannel: "",
+            activeChannel: "general",
             text: "",
-            settings: undefined
+            settings: undefined,
+            newChannel: ""
         }
     }
 
@@ -33,6 +34,22 @@ class ChannelList extends React.Component<{}, {
 		.then((response) => response.json())
 	}
 
+    async demoteAdmin(admin: number) {
+		return await fetch(`http://127.0.0.1:5000/channels/${this.state.activeChannel}/admin/${admin}`, { method: 'DELETE'})
+		.then((response) => response.json())
+	}
+
+    async newchannel() {
+		return await fetch("http://127.0.0.1:5000/channels", { 
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+			body: JSON.stringify({
+                "name": this.state.newChannel,
+                "owner": 7
+			})
+        })
+		.then((response) => response.json())
+	}
 
     componentDidMount() {
         this.getChannels()
@@ -75,7 +92,11 @@ class ChannelList extends React.Component<{}, {
         var settings = undefined;
         if (this.state.settings) {
             const admins = this.state.settings.admins.map((el: number) => (
-                <div>{el}<button>DELETE</button></div>
+                <div>
+                    {el}
+                    {/* <button>DEMOTE</button> */}
+                    <button onClick={() => this.demoteAdmin(el)}>DEMOTE</button> 
+                </div>
             ))
 
             settings = (<div className="Settings">
@@ -85,9 +106,19 @@ class ChannelList extends React.Component<{}, {
             </div>)
         }
 
+
 		return ( 
             <div>
                 {channels}
+                <form onSubmit={() => this.newchannel()}>
+                    <input 
+                        type="text" 
+                        value={this.state.newChannel}
+                        onChange={(e) => this.setState({newChannel: e.target.value})}
+                    />
+                    <input type="submit" value="add channel" />
+                </form>
+                {/* <button onClick={() => this.newchannel()}>Add Channel</button>  */}
                 <div className="ChatWindow">
                     {this.state.activeChannel}
                     {messages}
