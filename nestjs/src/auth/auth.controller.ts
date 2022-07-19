91fe325cc3a367aacf2c7f17cdff2d29fd837bfd
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-19 15:54:04 +0200

    return jwt token in nestjs and add jwt test endpoint

--- nestjs/src/auth/auth.controller.ts
+++ nestjs/src/auth/auth.controller.ts
@@ -8,28 +8,43 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 const util = require('node:util');
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController
-{
+{  
+	constructor(private authService: AuthService) {}
 	private readonly logger = new Logger(AuthController.name);
 	@Get('ft')
 	@UseGuards(AuthGuard('ft')) //before returning the get request this will try the ft strategy for authentication. If ft exists is checked during runtime, and will give Unknown authentication strategy "ft" if it doesn't exist.
-	async getLoginName(@Req() req: Request): Promise<any> { //the function name doesn't matter?
+	async login(@Req() req: Request): Promise<any> { //the function name doesn't matter?
 		const areq = await req;
 		//this.logger.log('get on auth/ft request:', util.inspect(areq, false, null, true)); /// HUGE!
 		//this.logger.log('get on auth/ft request:', util.inspect(areq.body, false, null, true)); // nothing
+
+		//it's always req.user, even though it's not a user at all :( 
 		const user = req.user;
 		this.logger.log('get on auth/ft user:', user);
 		this.logger.log('type of  user:', user.constructor.name);
 		//req.login.then(resp => {this.logger.log("in getLoginName:", resp.data.login);});
-		return user;
+		return this.authService.login(req.user);
+		//return user;
 	}
 	//async getUserFromDiscordLogin(@Req() req): Promise<any> {
 		//return req.user;
 	//}
-	@Get('callback')
+
+
+	//this will reauire a JWT token
+	@Get('test')
 	auth_callback() {
 		this.logger.log('auth callbackft\n');
+	}
+  
+	@UseGuards(JwtAuthGuard)
+	@Get('profile')
+	getProfile(@Req() req) {
+		return req.user;
 	}
 }
--- nestjs/src/auth/auth.module.ts
+++ nestjs/src/auth/auth.module.ts
@@ -2,20 +2,32 @@ import {
   //HttpModule,
   Module,
 } from '@nestjs/common';
-//import { AuthService } from './auth.service';
+import { AuthService } from './auth.service';
 //import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { FtStrategy } from './ft.strategy';
 import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports    : [
     //UsersModule,
     HttpModule,
+	JwtModule.registerAsync({
+		imports: [ConfigModule],
+		useFactory: async (configService: ConfigService) => ({
+			secret: configService.get('JWT_SECRET'),
+			signOptions: { expiresIn: '60s' },
+		}),
+		inject: [ConfigService],
+	}),
   ],
   providers  : [
-    //AuthService,
+    AuthService,
     FtStrategy,
+	JwtStrategy,
   ],
   controllers: [
     AuthController,
