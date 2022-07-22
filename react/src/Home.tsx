ed40e5ff67538c06b455e6de5e9693a4b3a93438
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-22 17:04:46 +0200

    make log in button nicer

--- react/src/Home.tsx
+++ react/src/Home.tsx
@@ -55,7 +55,7 @@ const Home = () =>
                   <Link to= {{pathname:"/pinkpong"}}><Button variant="contained">PinkPong</Button></Link>
                   <Link to= {{pathname:"/chat"}}><Button variant="contained">Chat</Button></Link>
                   <Link to= {{pathname:"/account"}}><Button variant="contained">My account</Button></Link>
-				  <a href="http://127.0.0.1:5000/auth/ft">Log in</a>
+				  <a href="http://127.0.0.1:5000/auth/ft"><Button variant="contained">Log in</Button></a>
             </header>
             </div>
 		</main>
