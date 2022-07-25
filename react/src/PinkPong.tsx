e219053265693d346baeed88f26e3a17c850ee88
Lars Verdoes <lverdoes@f1r1s19.codam.nl>
2022-07-25 08:50:42 +0200

    PinkPong canvas version

--- react/src/PinkPong.tsx
+++ react/src/PinkPong.tsx
@@ -1,244 +1,225 @@
-import React , {CSSProperties } from 'react';
-import './PinkPong.css';
+import React, { useEffect } from 'react';
+import Home from './Home';
+import {Route} from 'react-router-dom';
 
-const FieldHeight: number = 800;
-const FieldWidth: number = 600;
-const PaddleWidth: number = 100;
-const PaddleHeight: number = 25;
-const PaddleMovement: number = 10;
+export default function PinkPong() {
+	const paddleWidth = 100;
+	const paddleHeight = 25;
+	const ballWidth = 25;
+	const ballSpeed = 3;
+	const paddleSpeed = 10;
+	const maxScore = 3;
 
-interface PinkPongState {
-	field: number;
-	paddleX: number;
-	Opponent: number;
-	ballX: number;
-	ballY: number;
-	ballVX: number;
-	ballVY: number;
-	fieldWidth: number;
-	paddleWidth: number;
-	paddleHeight: number;
-	ballWidth: number;
-	scorePlayerOne: number;
-	scorePlayerTwo: number;
-}
+	var canvas: any;
+	var ctx: any;
 
-interface FieldProps {
-	margin: number;
-	width: number;
-}
-interface FieldState{}
+	var paddleP1X: number;
+	var paddleP1Y: number;
+	var paddleP2X: number;
+	var paddleP2Y: number;
 
-interface PinkPongProps {}
+	var ballX: number;
+	var ballY: number;
+	var ballVX: number;
+	var ballVY: number;
 
-interface BallProps {
-	x: number;
-	y: number;
-	width: number;
-}
+	var scoreP1: number = 0;
+	var scoreP2: number = 0;
 
-interface PaddleProps {
-	margin: number;
-	width: number;
-}
-
-interface PaddleState {}
-
-class Field extends React.Component<FieldProps, FieldState> {
-	render() {
-		const theStyle: CSSProperties = {position: 'absolute', left: this.props.margin + "px", top: "0px", width: this.props.width + "px"};
-		return (
-			<button className="field" style= {theStyle}/>
-			);
+	useEffect(() => {
+		canvas = document.getElementById("myCanvas");
+		if (!canvas)
+			throw ("error");
+		ctx = canvas.getContext("2d");
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		
+		setGame();
+		componentDidMount();
+		update();
+	}, []);
+	
+	function setGame() {
+		if (scoreP1 < maxScore && scoreP2 < maxScore) {
+			paddleP1X = 600;
+			paddleP1Y = 100;
+			paddleP2X = 600;
+			paddleP2Y = 1000;
+			ballX = 625;
+			ballY = 500;
+			var up = Math.random() - 0.5;
+			if (up > 0)
+				ballVY = 1;
+			else
+				ballVY = -1;
+			ballVX =  Math.random() - 0.5;
+			var length = Math.sqrt((ballVX * ballVX) + (ballVY * ballVY));
+			ballVX = ballVX / length;
+			ballVY = ballVY / length;
+			ballVX = ballVX * ballSpeed;
+			ballVY = ballVY * ballSpeed;
+		}
+		else {
+			//end the game
+			//	https://reactrouter.com/docs/en/v6/components/navigate
 		}
 	}
-class Paddle extends React.Component<PaddleProps, PaddleState> {
-	render() {
-		const theStyle: CSSProperties = {position: 'absolute', left: this.props.margin + "px", top: "0px", width: this.props.width + "px"};
-		return (
-			<button className="paddle" style= {theStyle}/>
-			);
-	}
-}
-class Opponent extends React.Component<PaddleProps, PaddleState> {
-	render() {
-		const theStyle: CSSProperties = {position: 'absolute', left: this.props.margin + "px", top: "0px", width: this.props.width + "px"};
-		return (
-			<button className="opponent" style= {theStyle}/>
-			);
-	}
-}
 
-class Ball extends React.Component<BallProps> {
-	render () {
-		const theStyle: CSSProperties = {position: 'absolute', left: this.props.x + "px", top: this.props.y + "px", width: this.props.width};
-		return (
-			<button className="ball" style ={theStyle}>
-			</button>
-		);
+	function componentDidMount() {
+		document.addEventListener("keydown", handleKeyPress, false);
+		setInterval(handleKeyPress, 1);
 	}
-}
+	function componentWillUnmount(){
+		document.removeEventListener("keydown", handleKeyPress, false);
+	}
 
-interface ScoreProps {
-	playerOneName: string;
-	playerTwoName: string;
-	playerOneScore: number;
-	playerTwoScore: number;
-}
-class ScoreBoard extends React.Component<ScoreProps> {
-	render () {
-		return (
-			<div className={"Score"}>
-				<p className={"playerName"}>{this.props.playerOneName}</p>
-				<p>{this.props.playerOneScore.toString()}</p>
-				<p className={"playerName"}>{this.props.playerTwoName}</p>
-				<p>{this.props.playerTwoScore.toString()}</p>
-			</div>
-		);
-	}
-}
-
-class PinkPong extends React.Component<PinkPongProps, PinkPongState> {
-	constructor(props: PinkPongProps) {
-		super(props);
-		this.state = {
-			field: 100,
-			paddleX: 450,
-			Opponent: 450,
-			ballX: 475,
-			ballY: 475,
-			ballVX: 0,
-			ballVY: 0,
-			fieldWidth: 800,
-			paddleWidth: 100,
-			paddleHeight: 50,
-			ballWidth: 50,
-			scorePlayerOne: 0,
-			scorePlayerTwo: 0,
-		};
-	}
-	handleKeyPress(event: KeyboardEvent) {
+	function handleKeyPress(event: KeyboardEvent) {
 		if (event.key === "k") {
-			if (this.state.paddleX < 800) {
-				this.setState({paddleX: this.state.paddleX + 10});
+			if (paddleP1X < canvas.width) {
+				paddleP1X = paddleP1X + paddleSpeed;
+				// this.setState({paddleX: this.state.paddleX + 10});
 			}
 		}
 		if (event.key === "j") {
-			if (this.state.paddleX > 100) {
-				this.setState({paddleX: this.state.paddleX - 10});
+			if (paddleP1X > 0) {
+				paddleP1X = paddleP1X - paddleSpeed;
+				// this.setState({paddleX: this.state.paddleX - 10});
 			}
 		}
 		if (event.key === "x") {
-			if (this.state.Opponent < 800) {
-				this.setState({Opponent: this.state.Opponent + 10});
+			if (paddleP2X < canvas.width) {
+				paddleP2X = paddleP2X + paddleSpeed;
+				// this.setState({Opponent: this.state.Opponent + 10});
 			}
 		}
 		if (event.key === "z") {
-			if (this.state.Opponent > 100) {
-				this.setState({Opponent: this.state.Opponent - 10});
+			if (paddleP2X > 0) {
+				paddleP2X = paddleP2X - paddleSpeed;
+				// this.setState({Opponent: this.state.Opponent - 10});
 			}
 		}
 	}
-	update() {
+
+	function update(){
 		/*	handle top side */
-		if (this.state.ballVY < 0 && this.state.ballY <= (100 + 34))
+		if ((ballX + ballWidth > paddleP1X && ballX <= paddleP1X + paddleWidth) && ballY - ballWidth <= paddleP1Y + paddleHeight)
 		{
-			if (this.state.ballY < 100 - 50)
-			{
-				/*	score a point */
-				this.setState({scorePlayerTwo: this.state.scorePlayerTwo + 1});
-				return this.reset();
-			}
-			else if (this.state.ballX + this.state.ballWidth > this.state.paddleX && this.state.ballX <= this.state.paddleX + this.state.paddleWidth)
-			{
-				/*	bounce top paddle */
-				this.setState({ballVY: this.state.ballVY * -1});
-			}
+			/*	bounce top paddle */
+			ballVY = ballVY * -1;
+		}
+		else if (ballY < paddleP1Y)
+		{
+			/*	score a point */
+			scoreP2 = scoreP2 + 1;
+			setGame();
 		}
 		/*	handle bottom side */
-		else if (this.state.ballVY > 0 && this.state.ballY >= (900 - 34 - 50))
+		if ((ballX + ballWidth > paddleP2X && ballX <= paddleP2X + paddleWidth) && ballY + ballWidth >= paddleP2Y)
 		{
-			if (this.state.ballY - 0 > 900)
-			{
-				/*	score a point */
-				this.setState({scorePlayerOne: this.state.scorePlayerOne + 1});
-				return this.reset();
-			}
-			else if (this.state.ballX + this.state.ballWidth > this.state.Opponent && this.state.ballX <= this.state.Opponent + this.state.paddleWidth)
-			{
-				/*	bounce bottom paddle */
-				this.setState({ballVY: this.state.ballVY * -1});
-			}
+			/*	bounce bottom paddle */
+			ballVY = ballVY * -1;
+		}
+		else if (ballY > paddleP2Y)
+		{
+			/*	score a point */
+			scoreP1 = scoreP1 + 1;
+			setGame();
 		}
 		/*	bounce east wall */
-		if (this.state.ballX + this.state.ballWidth > 900 && this.state.ballVX > 0)
+		if (ballX + ballWidth > canvas.width && ballVX > 0)
 		{
-			this.setState({ballVX: this.state.ballVX * -1});
+			ballVX = ballVX * -1;
 		}
 		/*	bounce west wall */
-		else if (this.state.ballX <= 100 && this.state.ballVX < 0)
+		else if (ballX - ballWidth <= 0 && ballVX < 0)
 		{
-			this.setState({ballVX: this.state.ballVX * -1});
+			ballVX = ballVX * -1;
 		}
-		
+
 		/*	calculate next position */
-		this.setState({ballY: this.state.ballY + this.state.ballVY,
-			ballX: this.state.ballX + this.state.ballVX});
-	}
-	reset () {
-		this.setState(
-			{
-
-				ballY: 475,
-				ballX: 475,
-				ballVX: 0,
-				ballVY: 3,
-				paddleX: 450,
-				Opponent: 450,
-			}
-		)
+		ballY = ballY + ballVY;
+		ballX = ballX + ballVX;
+		draw();
 	}
 
-	componentDidMount() {
-		document.addEventListener("keydown", this.handleKeyPress.bind(this), false);
-		setInterval(this.update.bind(this), 10);
+	function draw(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+		//background
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.fillStyle = 'pink';
+			ctx.rect(0, 0, canvas.width, canvas.height);
+			ctx.fill();
+		}
+		ctx.closePath();
+		ctx.restore();
+		//score P1
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.font = '48px serif';
+			ctx.fillText(scoreP1.toString(), 600, 300);
+		}
+		ctx.closePath();
+		ctx.restore();
+		//score P2
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.font = '48px serif';
+			ctx.fillText(scoreP2.toString(), 600, 800);
+		}
+		ctx.closePath();
+		ctx.restore();
+		//paddle P1
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.fillStyle = 'black';
+			ctx.strokeStyle = 'yellow';
+			ctx.rect(paddleP1X, paddleP1Y, paddleWidth, paddleHeight);
+			ctx.fill();
+			ctx.stroke();
+		}
+		ctx.closePath();
+		ctx.restore();
+		//paddle P2
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.fillStyle = 'white';
+			ctx.strokeStyle = 'yellow';
+			ctx.rect(paddleP2X, paddleP2Y, paddleWidth, paddleHeight);
+			ctx.fill();
+			ctx.stroke();
+		}
+		ctx.closePath();
+		ctx.restore();
+		//ball
+		ctx.save();
+		ctx.beginPath();
+		{
+			ctx.fillStyle = 'blue';
+			ctx.strokeStyle = 'yellow';
+			ctx.arc(ballX, ballY, ballWidth, -1.5 * Math.PI, 1.5 * Math.PI);
+			ctx.fill();
+			ctx.stroke();
+		}
+		ctx.closePath();
+		ctx.restore();
+		requestAnimationFrame(update);
 	}
-	componentWillUnmount(){
-		document.removeEventListener("keydown", this.handleKeyPress, false);
-	}
-	render() {
-		return (
-            <header className="PinkPong-header">
-			<div className="game">
-			<div className="game-board">
-			<Field
-			margin={this.state.field}
-			width={this.state.fieldWidth}
-			/>
-			<Paddle
-			margin={this.state.paddleX}
-			width={this.state.paddleWidth}
-			/>
-			<Opponent
-			margin={this.state.Opponent}
-			width={this.state.paddleWidth}
-			/>
-			<Ball
-			x={this.state.ballX}
-			y={this.state.ballY}
-			width={this.state.ballWidth}
-			/>
-			<ScoreBoard
-			playerOneName ={"Player1_TOP"}
-			playerTwoName ={"Player2_BOTTOM"}
-			playerOneScore ={this.state.scorePlayerOne}
-			playerTwoScore ={this.state.scorePlayerTwo}
-			/>
-			</div>
-			</div>
-            </header>
-		);
-	}
+
+
+  return (
+    <div>
+      <canvas
+        id="myCanvas"
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
+    </div>
+  );
 }
-
-export default PinkPong;
