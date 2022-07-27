0b414ad6cad93922dbaf6209c8d0d09fa3f6dc05
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-27 11:07:00 +0200

    don't show 2fa if not logged in

--- react/src/QR.tsx
+++ react/src/QR.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
-class ShowQR extends React.Component {
-	render () {
-		return (
+interface QRState {
+	li: boolean;
+}
+
+interface QRProps {
+}
+class ShowQR extends React.Component<QRProps, QRState> {
+	constructor(props: QRProps) {
+		super(props);
+		this.state = {
+			li: false,
+		}
+	}
+	async componentDidMount() {
+		const li =  fetch("http://127.0.0.1:5000/auth/amiloggedin", { 
+			method: 'GET',
+			credentials: 'include',
+		}).then(response => response.json());
+		this.setState({li: await li});
+	}
+	render() {
+		if (this.state.li)
+			return (
 			<div>
+			<div>
+			This is the google authenticator QR:
+				</div>
 			<img src="http://127.0.0.1:5000/2fa/generate" alt="alternatetext"/> 
 			</div>
-		)
+			)
+		else
+			return (<div>"You are not logged in, so I will not show you a QR"</div>);
 	}
 }
 
@@ -14,7 +39,6 @@ const Login = () =>
 {
 	return (
 		<main>
-		This is the google authenticator QR:
 			<ShowQR />
 		</main>
 	)
