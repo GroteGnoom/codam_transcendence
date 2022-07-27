ba9b6399e3805142a61b63912db9ca5aa61110db
Stef De rijk <sde-rijk@f1r1s17.codam.nl>
2022-07-27 10:40:54 +0200

    Game now 'runs' on websockets

--- nestjs/src/match/match.gateway.ts
+++ nestjs/src/match/match.gateway.ts
@@ -4,23 +4,158 @@ import {
   WebSocketServer
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-  
-  @WebSocketGateway({
-    cors: {
-      origin: '*',
-    },
-  })
-  export class MatchGateway {
-    // constructor(private channelsService: ChannelsService) {}
+
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+
+export class MatchGateway {
+    ballSpeed = 4;
+    paddleSpeed = 7;
+    maxScore = 3;
+    
+    paddleP1RelX: number;
+    paddleP1RelY: number;
+    paddleP2RelX: number;
+    paddleP2RelY: number;
+    
+    ballRelX: number;
+    ballRelY: number;
+    ballVX: number;
+    ballVY: number;
+    
+    winner: number = -1;
+    scoreP1: number = 0;
+    scoreP2: number = 0;
+    
+    fieldWidth = 1500;
+    fieldHeight = 1000;
+    paddleWidth = 100;
+    paddleHeight = 25;
+    ballWidth = 25;
   
     @WebSocketServer()
     server: Server;
   
-    @SubscribeMessage('updateBoard')
+    @SubscribeMessage('keyPressed')
     async handleSendMessage(client: Socket, payload: any): Promise<void> {
         console.log("Got a board update, emitting to listeners")
-        this.server.emit('boardUpdated', payload);
+        this.getPositions(payload.leftKeyPressedP1, payload.leftKeyPressedP2, payload.rightKeyPressedP1, payload.rightKeyPressedP2, payload.reset);
+    }
+
+    getPositions(leftKeyPressedP1: boolean, leftKeyPressedP2: boolean, rightKeyPressedP1: boolean, rightKeyPressedP2: boolean, reset: boolean) {
+      if (this.winner === 0){
+        /*	handle top side */
+        if ((this.ballRelX + this.ballWidth > this.paddleP1RelX && this.ballRelX < this.paddleP1RelX + this.paddleWidth) && (this.ballRelY >= this.paddleP1RelY + this.paddleHeight - 10 && this.ballRelY <= this.paddleP1RelY + this.paddleHeight) && this.ballVY < 0) {
+          /*	bounce top paddle */
+          this.ballVY = this.ballVY * -1;
+        }
+        else if (this.ballRelY < this.paddleP1RelY - 10) {
+          /*	score a point */
+          this.scoreP2 = this.scoreP2 + 1;
+          this.setGame(leftKeyPressedP1, leftKeyPressedP2, rightKeyPressedP1, rightKeyPressedP2, reset);
+        }
+        /*	handle bottom side */
+        if ((this.ballRelX + this.ballWidth >= this.paddleP2RelX && this.ballRelX <= this.paddleP2RelX + this.paddleWidth) && (this.ballRelY + this.ballWidth >= this.paddleP2RelY && this.ballRelY + this.ballWidth <= this.paddleP2RelY + 10) && this.ballVY > 0) {
+          /*	bounce bottom paddle */
+          this.ballVY = this.ballVY * -1;
+        }
+        else if (this.ballRelY + this.ballWidth > this.paddleP2RelY + this.paddleHeight + 10) {
+          /*	score a point */
+          this.scoreP1 = this.scoreP1 + 1;
+          this.setGame(leftKeyPressedP1, leftKeyPressedP2, rightKeyPressedP1, rightKeyPressedP2, reset);
+        }
+        /*	bounce east wall */
+        if (this.ballRelX + this.ballWidth > this.fieldWidth && this.ballVX > 0) {
+          this.ballVX = this.ballVX * -1;
+        }
+        /*	bounce west wall */
+        else if (this.ballRelX <= 0 && this.ballVX < 0) {
+          this.ballVX = this.ballVX * -1;
+        }
+        /*	calculate next position */
+        this.ballRelX = this.ballRelX + this.ballVX;
+        this.ballRelY = this.ballRelY + this.ballVY;
+        /*	calculate paddle positions */
+        if (leftKeyPressedP1 === true && this.paddleP1RelX > 1)
+          this.paddleP1RelX = this.paddleP1RelX - this.paddleSpeed;
+        if (rightKeyPressedP1 === true && this.paddleP1RelX + this.paddleWidth < this.fieldWidth - 1)
+          this.paddleP1RelX = this.paddleP1RelX + this.paddleSpeed;
+        if (leftKeyPressedP2 === true && this.paddleP2RelX > 1)
+          this.paddleP2RelX = this.paddleP2RelX - this.paddleSpeed;
+        if (rightKeyPressedP2 === true && this.paddleP2RelX + this.paddleWidth < this.fieldWidth - 1)
+          this.paddleP2RelX = this.paddleP2RelX + this.paddleSpeed;
+        }
+        else if (this.winner === -1) {
+          this.winner = 0;
+          this.setGame(leftKeyPressedP1, leftKeyPressedP2, rightKeyPressedP1, rightKeyPressedP2, reset);
+        }
+        else
+          this.setGame(leftKeyPressedP1, leftKeyPressedP2, rightKeyPressedP1, rightKeyPressedP2, reset);
+        //send relative coordinates of paddles and ball to frontend
+        this.server.emit('boardUpdated', { 
+          "ballRelX": this.ballRelX, 
+          "ballRelY": this.ballRelY, 
+          "paddleP1RelX": this.paddleP1RelX, 
+          "paddleP1RelY": this.paddleP1RelY, 
+          "paddleP2RelX": this.paddleP2RelX, 
+          "paddleP2RelY": this.paddleP2RelY, 
+          "scoreP1": this.scoreP1, 
+          "scoreP2": this.scoreP2,
+          "winner": this.winner
+        });
+    }
+
+    setGame(leftKeyPressedP1: boolean, leftKeyPressedP2: boolean, rightKeyPressedP1: boolean, rightKeyPressedP2: boolean, reset: boolean) {
+      console.log(reset);
+      if (this.scoreP1 < this.maxScore && this.scoreP2 < this.maxScore) {
+        this.paddleP1RelX = this.fieldWidth / 2 - this.paddleWidth / 2;
+        this.paddleP1RelY = 10;
+        this.paddleP2RelX = this.fieldWidth / 2 - this.paddleWidth / 2;
+        this.paddleP2RelY = this.fieldHeight - 10 - this.paddleHeight;
+        this.ballRelX = this.fieldWidth / 2 - this.ballWidth / 2;
+        this.ballRelY = this.fieldHeight / 2 - this.ballWidth / 2;
+        leftKeyPressedP1 = false;
+        rightKeyPressedP1 = false;
+        leftKeyPressedP2 = false;
+        rightKeyPressedP2 = false;
+        var up = Math.random() - 0.5;
+        if (up > 0)
+          this.ballVY = 1;
+        else
+          this.ballVY = -1;
+        this.ballVX =  Math.random() - 0.5;
+        var length = Math.sqrt((this.ballVX * this.ballVX) + (this.ballVY * this.ballVY));
+        this.ballVX = this.ballVX / length;
+        this.ballVY = this.ballVY / length;
+        this.ballVX = this.ballVX * this.ballSpeed;
+        this.ballVY = this.ballVY * this.ballSpeed;
+      }
+      else if (reset === true) {
+        console.log("reset");
+        this.winner = -1;
+        this.scoreP1 = 0;
+        this.scoreP2 = 0;
+      }
+      else {
+        if (this.scoreP1 === this.maxScore)
+          this.winner = 1;
+        else
+          this.winner = 2;
+        this.server.emit('boardUpdated', { 
+          "ballRelX": this.ballRelX, 
+          "ballRelY": this.ballRelY, 
+          "paddleP1RelX": this.paddleP1RelX, 
+          "paddleP1RelY": this.paddleP1RelY, 
+          "paddleP2RelX": this.paddleP2RelX, 
+          "paddleP2RelY": this.paddleP2RelY, 
+          "scoreP1": this.scoreP1, 
+          "scoreP2": this.scoreP2,
+          "winner": this.winner 
+        });
+      }
     }
   }
-  
-  
--- react/src/PinkPong.tsx
+++ react/src/PinkPong.tsx
@@ -8,41 +8,28 @@ export default function PinkPong() {
 	const paddleWidth = 100;
 	const paddleHeight = 25;
 	const ballWidth = 25;
-	const ballSpeed = 4;
-	const paddleSpeed = 7;
-	const maxScore = 3;
+	
+	let canvas: any;
+	let ctx: any;
+	
+	let paddleP1X: number;
+	let paddleP1Y: number;
+	let paddleP2X: number;
+	let paddleP2Y: number;
+	
+	let ballX: number;
+	let ballY: number;
+	
+	let leftKeyPressedP1: boolean;
+	let rightKeyPressedP1: boolean;
+	let leftKeyPressedP2: boolean;
+	let rightKeyPressedP2: boolean;
 
-	var canvas: any;
-	var ctx: any;
-	
-	var paddleP1X: number;
-	var paddleP1RelX: number;
-	var paddleP1Y: number;
-	var paddleP1RelY: number;
-	var paddleP2X: number;
-	var paddleP2RelX: number;
-	var paddleP2Y: number;
-	var paddleP2RelY: number;
-	
-	var ballX: number;
-	var ballRelX: number;
-	var ballY: number;
-	var ballRelY: number;
-	var ballVX: number;
-	var ballVY: number;
-	
-	var leftKeyPressedP1: boolean;
-	var rightKeyPressedP1: boolean;
-	var leftKeyPressedP2: boolean;
-	var rightKeyPressedP2: boolean;
-	
-	var gameEnd: boolean = false;
-	var scoreP1: number = 0;
-	var scoreP2: number = 0;
-	
+	let setTimeOut: boolean;
+
+	const webSocket: any = useRef(null); // useRef creates an object with a 'current' property
 	let navigate = useNavigate();
-	const webSocket: any = useRef(null); // useRef creates an object with a 'current' property 
-	
+
 	useEffect(() => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		canvas = document.getElementById("myCanvas");
@@ -51,40 +38,27 @@ export default function PinkPong() {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		ctx = canvas.getContext("2d");
 		
-		setGame();
 		componentDidMount();
-		update();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		setTimeOut = false;
+		
+		console.log('Opening WebSocket');
+		webSocket.current = io('ws://localhost:5000'); // open websocket connection with backend
+		webSocket.current.emit("keyPressed", {
+			"leftKeyPressedP1": false,
+			"rightKeyPressedP1": false,
+			"leftKeyPressedP2": false,
+			"rightKeyPressedP2": false,
+			"reset": false
+		})
+		webSocket.current.on("boardUpdated", getCoordinates ) // subscribe on backend events
+
+		return () => {
+			console.log('Closing WebSocket');
+			webSocket.current.close();
+		}
 	}, []);
-
-	function setGame() {
-		if (scoreP1 < maxScore && scoreP2 < maxScore) {
-			paddleP1RelX = fieldWidth / 2 - paddleWidth / 2;
-			paddleP1RelY = 10;
-			paddleP2RelX = fieldWidth / 2 - paddleWidth / 2;
-			paddleP2RelY = fieldHeight - 10 - paddleHeight;
-			ballRelX = fieldWidth / 2 - ballWidth / 2;
-			ballRelY = fieldHeight / 2 - ballWidth / 2;
-			leftKeyPressedP1 = false;
-			rightKeyPressedP1 = false;
-			leftKeyPressedP2 = false;
-			rightKeyPressedP2 = false;
-			var up = Math.random() - 0.5;
-			if (up > 0)
-				ballVY = 1;
-			else
-				ballVY = -1;
-			ballVX =  Math.random() - 0.5;
-			var length = Math.sqrt((ballVX * ballVX) + (ballVY * ballVY));
-			ballVX = ballVX / length;
-			ballVY = ballVY / length;
-			ballVX = ballVX * ballSpeed;
-			ballVY = ballVY * ballSpeed;
-		}
-		else {
-			gameEnd = true;
-		}
-	}
-
+	
 	function componentDidMount() {
 		document.addEventListener("keydown", handleKeyPress, false);
 		document.addEventListener("keyup", handleKeyRelease, false);
@@ -114,100 +88,39 @@ export default function PinkPong() {
 			rightKeyPressedP2 = false;
 			leftKeyPressedP2 = true;
 		}
+		webSocket.current.emit("keyPressed", {
+			"leftKeyPressedP1": leftKeyPressedP1,
+			"rightKeyPressedP1": rightKeyPressedP1,
+			"leftKeyPressedP2": leftKeyPressedP2,
+			"rightKeyPressedP2": rightKeyPressedP2,
+			"reset": false
+		})
 	}
-
-	function update(){
-		if (gameEnd === false){
-			/*	handle top side */
-			if ((ballRelX + ballWidth > paddleP1RelX && ballRelX < paddleP1RelX + paddleWidth) && (ballRelY >= paddleP1RelY + paddleHeight - 15 && ballRelY <= paddleP1RelY + paddleHeight) && ballVY < 0) {
-				/*	bounce top paddle */
-				ballVY = ballVY * -1;
-			}
-			else if (ballRelY < paddleP1RelY - 10) {
-				/*	score a point */
-				scoreP2 = scoreP2 + 1;
-				setGame();
-			}
-			/*	handle bottom side */
-			if ((ballRelX + ballWidth >= paddleP2RelX && ballRelX <= paddleP2RelX + paddleWidth) && (ballRelY + ballWidth >= paddleP2RelY && ballRelY + ballWidth <= paddleP2RelY + 15) && ballVY > 0) {
-				/*	bounce bottom paddle */
-				ballVY = ballVY * -1;
-			}
-			else if (ballRelY + ballWidth > paddleP2RelY + paddleHeight + 10) {
-				/*	score a point */
-				scoreP1 = scoreP1 + 1;
-				setGame();
-			}
-			/*	bounce east wall */
-			if (ballX + ballWidth > getFieldX() + fieldWidth && ballVX > 0) {
-				ballVX = ballVX * -1;
-			}
-			/*	bounce west wall */
-			else if (ballX - getFieldX() <= 0 && ballVX < 0) {
-				ballVX = ballVX * -1;
-			}
-			/*	calculate next position */
-			ballRelX = ballRelX + ballVX;
-			ballRelY = ballRelY + ballVY;
-			ballX = getFieldX() + ballRelX;
-			ballY = getFieldY() + ballRelY;
-			/*	calculate paddle positions */
-			if (leftKeyPressedP1 === true && paddleP1RelX > 0)
-				paddleP1RelX = paddleP1RelX - paddleSpeed;
-			if (rightKeyPressedP1 === true && paddleP1RelX + paddleWidth < fieldWidth)
-				paddleP1RelX = paddleP1RelX + paddleSpeed;
-			paddleP1X = getFieldX() + paddleP1RelX;
-			paddleP1Y = getFieldY() + paddleP1RelY;
-			if (leftKeyPressedP2 === true && paddleP2RelX > 0)
-				paddleP2RelX = paddleP2RelX - paddleSpeed;
-			if (rightKeyPressedP2 === true && paddleP2RelX + paddleWidth < fieldWidth)
-				paddleP2RelX = paddleP2RelX + paddleSpeed;
-			paddleP2X = getFieldX() + paddleP2RelX;
-			paddleP2Y = getFieldY() + paddleP2RelY;
-		
-			draw();
+	
+	function getCoordinates(payload: any) {
+		if (payload.winner !== 0 && payload.winner !== -1) {
+			console.log("EndGame");
+			EndGame(payload.winner, payload.scoreP1, payload.scoreP2);
+		}
+		else if (payload.winner === 0) {
+			ballX = getFieldX() + payload.ballRelX;
+			ballY = getFieldY() + payload.ballRelY;
+			paddleP1X = getFieldX() + payload.paddleP1RelX;
+			paddleP1Y = getFieldY() + payload.paddleP1RelY;
+			paddleP2X = getFieldX() + payload.paddleP2RelX;
+			paddleP2Y = getFieldY() + payload.paddleP2RelY;
+			draw(ballX, ballY, paddleP1X, paddleP1Y, paddleP2X, paddleP2Y, payload.scoreP1, payload.scoreP2);
 		}
 	}
 	
-	function drawRectangle(x:number, y:number, width:number, height:number, fillColour:string, strokeColour:string) {
-		ctx.save();
-		ctx.beginPath();
-	
-		ctx.fillStyle = fillColour;
-		ctx.strokeStyle = strokeColour;
-		ctx.rect(x, y, width, height);
-		ctx.fill();
-		ctx.stroke();
-	
-		ctx.closePath();
-		ctx.restore();
-	}
-
-	function drawText(text:string, x:number, y:number, font:string) {
-		ctx.save();
-		ctx.beginPath();
-	
-		ctx.font = font;
-		ctx.fillText(text, x, y);
-	
-		ctx.closePath();
-		ctx.restore();
-	}
-
-	function getFieldX() {
-		return (canvas.width / 2 - fieldWidth / 2);
-	}
-
-	function getFieldY() {
-		return (canvas.height / 2 - fieldHeight / 2);
-	}
-
-	function draw(){
+	function draw(ballX: number, ballY: number, paddleP1X: number, paddleP1Y: number, paddleP2X: number, paddleP2Y: number, scoreP1: number, scoreP2: number){
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+		ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		
 		//background
-		drawRectangle(0, 0, canvas.width, canvas.height, 'lightpink', 'lightpink');
+		drawRectangle(0, 0, window.innerWidth, window.innerHeight, 'lightpink', 'lightpink');
 		//field
 		drawRectangle(getFieldX(), getFieldY(), fieldWidth, fieldHeight, 'pink', 'darkpink');
 		//score P1
@@ -221,63 +134,94 @@ export default function PinkPong() {
 		//ball
 		drawRectangle(ballX, ballY, ballWidth, ballWidth, 'blue', 'yellow');
 
-		if (webSocket.current) { // to prevent null pointer errors
-			webSocket.current.emit("updateBoard", 
-				{
-					"ballX": ballX, 
-					"ballY": ballY, 
-				}) // example payload that can be sent as websocket event
-		}
-
-		if (gameEnd === true)
-			endGame();
-		else
-			requestAnimationFrame(update);
+		webSocket.current.emit("keyPressed", {
+			"leftKeyPressedP1": leftKeyPressedP1,
+			"rightKeyPressedP1": rightKeyPressedP1,
+			"leftKeyPressedP2": leftKeyPressedP2,
+			"rightKeyPressedP2": rightKeyPressedP2,
+			"reset": false
+		})
+	}
+		
+	function getFieldX() {
+		return (window.innerWidth / 2 - fieldWidth / 2);
 	}
 	
-	function endGame() {
-		setTimeout(() => {
-			navigate("/", { replace: true });}, 5000); //Reroute to home page after 5 seconds
-		
-		getWindowSize();
+	function getFieldY() {
+		return (window.innerHeight / 2 - fieldHeight / 2);
+	}
+	
+	function drawRectangle(x:number, y:number, width:number, height:number, fillColour:string, strokeColour:string) {
+		ctx.save();
+		ctx.beginPath();
+
+		ctx.fillStyle = fillColour;
+		ctx.strokeStyle = strokeColour;
+		ctx.rect(x, y, width, height);
+		ctx.fill();
+		ctx.stroke();
+
+		ctx.closePath();
+		ctx.restore();
 	}
 
-	function getWindowSize() {
+	function drawText(text:string, x:number, y:number, font:string) {
+		ctx.save();
+		ctx.beginPath();
+
+		ctx.font = font;
+		ctx.fillText(text, x, y);
+		
+		ctx.closePath();
+		ctx.restore();
+	}
+
+	function EndGame (winner: number, scoreP1: number, scoreP2: number) {
+		getWindowSize(winner, scoreP1, scoreP2);
+		if (setTimeOut === false) {
+			setTimeOut = true;
+			setTimeout(() => {
+				webSocket.current.emit("keyPressed", {
+				"leftKeyPressedP1": leftKeyPressedP1,
+				"rightKeyPressedP1": rightKeyPressedP1,
+				"leftKeyPressedP2": leftKeyPressedP2,
+				"rightKeyPressedP2": rightKeyPressedP2,
+				"reset": true
+				})
+				setTimeout(() => {
+				console.log('Closing WebSocket EndGame');
+				webSocket.current.close();
+				navigate("/", { replace: true });}, 100)}, 5000); //Reroute to home page after 5 seconds
+		}
+		webSocket.current.emit("keyPressed", {
+			"leftKeyPressedP1": leftKeyPressedP1,
+			"rightKeyPressedP1": rightKeyPressedP1,
+			"leftKeyPressedP2": leftKeyPressedP2,
+			"rightKeyPressedP2": rightKeyPressedP2,
+			"reset": false
+			})
+	};
+
+	function getWindowSize(winner: number, scoreP1: number, scoreP2: number) {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
-		drawEndScreen();
+		drawEndScreen(winner, scoreP1, scoreP2);
 	}
 
-	function drawEndScreen() {
+	function drawEndScreen(winner: number, scoreP1: number, scoreP2: number) {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		
 		//background
 		drawRectangle(0, 0, canvas.width, canvas.height, 'lightpink', 'lightpink');
 		//Show winner
-		if (scoreP1 === maxScore)
+		if (winner === 1)
 			drawText("Player 1 has won!", getFieldX() + 440, getFieldY() + 400, '64px serif');
 		else
 			drawText("Player 2 has won!", getFieldX() + 440, getFieldY() + 400, '64px serif');
 		//Show score
 		drawText(scoreP1.toString() + " - " + scoreP2.toString(), getFieldX() + 600, getFieldY() + 600, '48px serif');
-		requestAnimationFrame(getWindowSize);
 	}
-
-	function updateBoardState(boardState: any) {		
-		console.log("New board", boardState)
-		// handle updated state here
-	}
-
-    useEffect(() => { // makes sure websocket is opened when opening page, and closed when exiting page
-        console.log('Opening WebSocket');
-        webSocket.current = io('ws://localhost:5000'); // open websocket connection with backend
-		webSocket.current.on("boardUpdated", updateBoardState ) // subscribe on backend events
-        return () => {
-            console.log('Closing WebSocket');
-            webSocket.current.close();
-        }
-    });
-
+	
 	return (
 		<div>
 			<canvas
