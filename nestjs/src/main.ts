b4cd27eb8c40f5e9a2df4d39c856c4556cdf89b1
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-29 14:48:40 +0200

    add cors feedback to nestjs main

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -4,6 +4,7 @@ import { useContainer } from 'class-validator'
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as cors from 'cors'
 
 async function bootstrap() {
 	const configApp = await NestFactory.create(AppModule);
@@ -22,12 +23,21 @@ async function bootstrap() {
 		  saveUninitialized: false,
 	  }),
   );
-  //app.enableCors();
-  app.enableCors({
-	  origin: true,
-    "credentials": true,
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-  await app.listen(5000);
+    var allowed = ['http://127.0.0.1:3000', 'https://127.0.0.1:5000']
+    var corsOptions = {
+      origin: (origin, callback) => {
+		  if (!origin) {
+			  console.log('origin: ', origin);  // => undefined
+			  callback(null);
+			  return;
+		  }
+          var originIsAllowed = allowed.indexOf(origin) !== -1;
+          console.log('ORIGIN: ', origin);  // => undefined
+          callback(originIsAllowed ? null : 'Bad Request', originIsAllowed)
+      },
+      credentials:true
+    }
+	app.use(cors(corsOptions));
+	await app.listen(5000);
 }
 bootstrap();
