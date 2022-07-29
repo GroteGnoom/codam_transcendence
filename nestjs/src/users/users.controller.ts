56b8145d1013592491fd5027aedfc696c9e8a5d4
iboeters <irisboeters@msn.com>
2022-07-29 09:10:49 +0200

    unique intraname and new format

--- nestjs/src/users/users.controller.ts
+++ nestjs/src/users/users.controller.ts
@@ -1,52 +1,62 @@
 import {
-	Body,
-	Controller,
-	Get,
-	Param,
-	ParseIntPipe,
-	Post,
-	UsePipes,
-	ValidationPipe,
-	Logger,
-	Req,
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Req,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
-import { CreateUserDto } from 'src/users/users.dtos';
-import { UsersService } from 'src/users/users.service';
+import {ReservedOrUserEventNames} from 'socket.io/dist/typed-events';
+import {CreateUserDto} from 'src/users/users.dtos';
+import {UsersService} from 'src/users/users.service';
 
 @Controller('users')
 export class UsersController {
-	private readonly logger = new Logger(UsersController.name);
-	constructor(private readonly userService: UsersService) {}
+  private readonly logger = new Logger(UsersController.name);
+  constructor(private readonly userService: UsersService) {}
 
-	@Get()
-	getUsers() {
-		this.logger.log('getting users\n');
-		return this.userService.getUsers();
-	}
+  @Get()
+  getUsers() {
+    this.logger.log('getting users\n');
+    return this.userService.getUsers();
+  }
 
-	@Get('id')
-	findUsersById(@Req() req: any) {
-		const userID = req.session.userId;
-		return this.userService.findUsersById(userID);
-	}
+  @Get('id')
+  findUsersById(@Req() req: any) {
+    const userID = req.session.userId;
+    return this.userService.findUsersById(userID);
+  }
 
-	@Post('create')
-	@UsePipes(ValidationPipe)
-	createUsers(@Body() createUserDto: CreateUserDto) {
-		this.logger.log('Creating user...')
-		return this.userService.createUser(createUserDto);
-	}
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    this.logger.log('Creating user...')
+    return this.userService.createUser(createUserDto);
+  }
 
-	@Post('setusername')
-	@UsePipes(ValidationPipe)
-	setUsername(username: string) {
-		this.logger.log("setUsername called");
-		return this.userService.setUsername(username);
-	}
+  @Put('signupuser')
+  @UsePipes(ValidationPipe)
+  signUpUser(username: string, intraName: string) {
+    this.logger.log('Signup user...');
+    this.logger.log(intraName, username);
+    return this.userService.signUpUser(username, intraName);
+  }
 
-	@Get('intraname')
-	getIntraname(@Req() req: any) {
-		const userId = req.session.userId;
-		return this.userService.getIntraname(userId);
-	}
+  @Post('setusername')
+  @UsePipes(ValidationPipe)
+  setUsername(username: string) {
+    this.logger.log("setUsername called");
+    return this.userService.setUsername(username);
+  }
+
+  @Get('intraname')
+  getIntraname(@Req() req: any) {
+    const userId = req.session.userId;
+    return this.userService.getIntraname(userId);
+  }
 }
--- nestjs/src/users/users.dtos.ts
+++ nestjs/src/users/users.dtos.ts
@@ -1,21 +1,16 @@
-import { IsNotEmpty } from "class-validator";
-import { uniqueUser } from './uniqueUser';
+import {IsNotEmpty} from "class-validator";
+import {userStatus} from '../typeorm/user.entity'
+import {uniqueUser} from './uniqueUser';
 
-export enum userStatus {
-	Online = "online",
-	Offline = "offline",
-	InGame = "inGame",
-}
 export class CreateUserDto {
-	@IsNotEmpty()
-	// @uniqueUser({message: 'Username already exists. Choose another name.'})
-	// TODO: add more validation: no special characters etc.
-	username: string;
+  @IsNotEmpty()
+  // @uniqueUser({message: 'Username already exists. Choose another name.'})
+  // TODO: add more validation: no special characters etc.
+  username: string;
 
-	@IsNotEmpty()
-	intraName: string;
+  @IsNotEmpty() intraName: string;
 
-	status: userStatus;
+  status: userStatus;
 
-	isTfaEnabled: boolean;
+  isTfaEnabled: boolean;
 }
