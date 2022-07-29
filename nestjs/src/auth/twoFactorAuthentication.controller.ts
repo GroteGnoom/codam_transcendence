8668dedc591e137732aec60f32f0686203ac65ba
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-29 11:12:37 +0200

    fix 2fa authentication frontend

--- nestjs/src/auth/twoFactorAuthentication.controller.ts
+++ nestjs/src/auth/twoFactorAuthentication.controller.ts
@@ -17,6 +17,7 @@ import { Response, Request } from 'express';
 import { User } from '../typeorm/user.entity';
 import { UsersService } from '../users/users.service';
 import { TwoFactorAuthenticationDto } from './dto'
+import * as rawbody from 'raw-body';
  
 interface RequestWithUser extends Request {
   user: User;
@@ -35,14 +36,14 @@ export class TwoFactorAuthenticationController {
 	@Post('generate')
 	//@UseGuards(JwtAuthenticationGuard)
 	async register(@Res() response: Response, @Req() request: RequestWithUser) {
-		const { otpauthUrl } = await this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(request.user.toString());
+		const { otpauthUrl } = await this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(request.user);
 
 		return this.twoFactorAuthenticationService.pipeQrCodeStream(response, otpauthUrl);
 	}
 	@Get("generate")
 	async register_current_user(@Res() response: Response, @Req() request: RequestWithUser) {
 		const user = await this.userService.findUsersById(request.session.userId);
-		const { otpauthUrl } = await this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(user.intraName);
+		const { otpauthUrl } = await this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(user);
 
 		await this.userService.turnOnTwoFactorAuthentication(request.session.userId);
 		return this.twoFactorAuthenticationService.pipeQrCodeStream(response, otpauthUrl);
@@ -74,16 +75,21 @@ export class TwoFactorAuthenticationController {
 		@Body() { twoFactorAuthenticationCode } : TwoFactorAuthenticationDto
 	) {
 		const user = await this.userService.findUsersById(request.session.userId);
-		this.logger.log("received 2fa code: ", twoFactorAuthenticationCode);
+		this.logger.log('received 2fa code: ' + twoFactorAuthenticationCode);
 		const secret = user.twoFactorAuthenticationSecret;
+		this.logger.log("secret: ", secret);
 		const isCodeValid = this.twoFactorAuthenticationService.isTwoFactorAuthenticationCodeValid(
 			twoFactorAuthenticationCode, secret
 		);
 		if (!isCodeValid) {
+			this.logger.log('2fa code is incorrect :(');
 			throw new UnauthorizedException('Wrong authentication code');
 		}
+		this.logger.log('2fa code is correct');
 		request.session.tfa_validated = true;
 
-		return request.session.tfa_validated;
+		//return request.session.tfa_validated;
+		this.logger.log('going to respond');
+		return "I am a banana";
 	}
 }
