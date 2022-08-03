b387c6233d33654e805749adfdb7de20d74c45a3
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-08-03 15:53:08 +0200

    actually add the session guard file

---
+++ nestjs/src/auth/session.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+	console.log('userId in guard: ', request.session.userId);
+    return (request.session.logged_in);
+  }
+}
