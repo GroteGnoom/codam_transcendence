4c73fdbb1099a1313ee21bca981b25707156f3dc
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-08-15 11:15:15 +0200

    also emit games when there are no games

--- nestjs/src/match/match.gateway.ts
+++ nestjs/src/match/match.gateway.ts
@@ -337,19 +337,18 @@ export class MatchGateway {
 
   async emitGames() {
 	  let gameArray = [];
-	  this.currentGameStates.forEach((gameState, matchID) => {
+	  for (const [matchId, gameState] of this.currentGameStates) {
 		  console.log('pushing match');
-		  gameState.getUsernames().then( () => {
-			  console.log('after get usernames');
-			  gameArray.push({userName1: gameState.userName1,
-							 userName2: gameState.userName2, 
-							 matchID: gameState.matchID,
-			  });
-			  console.log('emitting array');
-			  console.log('gameArray', gameArray);
-			  this.server.emit('matches', gameArray);
-		  })
-	  });
+		  await gameState.getUsernames();
+		  console.log('after get usernames');
+		  gameArray.push({userName1: gameState.userName1,
+						 userName2: gameState.userName2, 
+						 matchID: gameState.matchID,
+		  });
+	  }
+	  console.log('emitting array');
+	  console.log('gameArray', gameArray);
+	  this.server.emit('matches', gameArray);
   }
 
   @SubscribeMessage('getGames')
@@ -376,7 +375,7 @@ export class MatchGateway {
       this.currentGameStates.set(matchID, new gameState(this.userService, payload.Player1, payload.Player2, matchID, payload.PinkPong));
       
       console.log("currentGameState size: ", this.currentGameStates.size);
-	  console.log('emitting matches');
+	  console.log('emitting matches at startGame');
 	  this.emitGames();
     }
   }
@@ -399,6 +398,7 @@ export class MatchGateway {
       if (emitMessage.winner === 1 || emitMessage.winner === 2) {
         this.matchService.storeResult(emitMessage.matchID, emitMessage.scoreP1, emitMessage.scoreP2);
         this.currentGameStates.delete(matchID);
+		console.log('emitting games at loop, game ended');
 		this.emitGames();
       }
     });
